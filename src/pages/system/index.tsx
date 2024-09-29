41bb5e9589f5af6166184931399474eda64d90ea
“linchao” <lc13579443@qq.com>
2024-09-30 00:36:46 +0800

    系统管理

--- src/pages/system/index.tsx
+++ src/pages/system/index.tsx
@@ -1,9 +1,8 @@
 import { Component, PropsWithChildren } from 'react'
-import { View, Text } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
+import Taro from '@tarojs/taro';
+import { View } from '@tarojs/components'
+import { AtGrid, AtDivider } from "taro-ui"
 import TabBar from '../../component/TabBar';
-
-import "taro-ui/dist/style/components/button.scss" // 按需引入
 import './index.scss'
 
 export default class System extends Component<PropsWithChildren> {
@@ -14,16 +13,61 @@ export default class System extends Component<PropsWithChildren> {
   componentDidShow () { }
 
   componentDidHide () { }
-
+  private handleClick = (value, index): void => {
+    Taro.showModal({
+      title: '提示',
+      content: `Value: ${JSON.stringify(value)}, Index: ${index}`,
+      showCancel: false
+    })
+  }
   render () {
     return (
       <View className='<%= pageName %>'>
-        <Text>Hello world!</Text>
-        <AtButton type='primary'>I need Taro UI</AtButton>
-        <Text>Taro UI 支持 Vue 了吗？</Text>
-        <AtButton type='primary' circle={true}>支持</AtButton>
-        <Text>共建？</Text>
-        <AtButton type='secondary' circle={true}>来</AtButton>
+        <AtDivider content='权限管理' />
+        <AtGrid onClick={this.handleClick} data={
+          [
+            {
+              iconInfo: { size: 30,  color: '#F00', value: 'settings'},
+              value: '系统管理',
+              url: '支持自定义字段'
+            },
+            {
+              iconInfo: { size: 30,  color: '#F00', value: 'user'},
+              value: '角色管理',
+              url: '支持自定义字段'
+            },
+          ]
+        } />
+        <AtDivider content='系统日志' />
+        <AtGrid onClick={this.handleClick} data={
+          [
+            {
+              iconInfo: { size: 30,  color: '#F00', value: 'download-cloud'},
+              value: '接口访问日志',
+              url: '支持自定义字段'
+            },
+            {
+              iconInfo: { size: 30,  color: '#F00', value: 'external-link'},
+              value: '外部接口日志',
+              url: '支持自定义字段'
+            },
+            {
+              iconInfo: { size: 30,  color: '#F00', value: 'numbered-list'},
+              value: '数据变更日志',
+              url: '支持自定义字段'
+            },
+            {
+              iconInfo: { size: 30,  color: '#F00', value: 'clock'},
+              value: '定时任务日志',
+              url: '支持自定义字段'
+            },
+            {
+              iconInfo: { size: 30,  color: '#F00', value: 'bullet-list'},
+              value: '系统接口路由',
+              url: '支持自定义字段'
+            },
+          ]
+        } />
         <TabBar />
       </View>
     )
